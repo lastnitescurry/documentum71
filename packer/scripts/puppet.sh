843cb32989849075cc44a88670f7c28ddfa74499
lastnitescurry <Brian.Dinneen@gmail.com>
2016-04-14 17:17:51 +0100

    Puppet Install and Configurations.
    Add modules and gems

--- packer/scripts/puppet.sh
+++ packer/scripts/puppet.sh
@@ -16,4 +16,14 @@ enabled=1
 gpgcheck=0
 EOM
 
+yum -y update
 yum -y install puppet facter ruby-shadow
+
+gem install hiera-eyaml -v 2.0.8
+gem install deep_merge  -v 1.0.1
+
+puppet module install puppetlabs-stdlib
+puppet module install nanliu-staging
+puppet module install puppetlabs-concat
+puppet module install puppetlabs-tomcat
+puppet module install ccin2p3-etc_services
