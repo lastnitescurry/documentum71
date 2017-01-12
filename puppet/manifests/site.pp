aef0c65e4cb7b8022b258cdd14821e60acb6bcb0
garethmcmillan <gareth.mcmillan@gmail.com>
2017-01-12 09:56:07 +0000

    lets build the machine based upon hostname

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -5,5 +5,11 @@
 # https://docs.puppet.com/hiera/latest/complete_example.html
 #
 node default {
+
+## we need to figure out how the machine is built
+#(this bit I was missing and mucked me up at hte end of a log day)
+
+  $role = hiera('role',{})
+  
   hiera_include('classes')
 }
